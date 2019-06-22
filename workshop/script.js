fd0c62b8762c1a455275234951faeb7f93b31fdd
Roman Zubko <skcorpoma@gmail.com>
2019-06-22 20:07:27 +0300

    dayfirst/xmlhttp

---
+++ workshop/script.js
@@ -0,0 +1,45 @@
+const searchForm = document.querySelector("#search-form");
+const movie = document.querySelector("#movies");
+function apiSearch(e) {
+  e.preventDefault();
+  //save value -> .value
+  const searchText = document.querySelector(".form-control").value,
+    server =
+      "https://api.themoviedb.org/3/search/multi?api_key=03df5e3d94a77bed0b06a5ec7a92cc71&language=ru&query=" +
+      searchText;
+  requestApi("GET", server);
+}
+
+searchForm.addEventListener("submit", apiSearch);
+
+function requestApi(method, url) {
+  const request = new XMLHttpRequest();
+  request.open(method, url);
+  request.send();
+
+  request.addEventListener("readystatechange", () => {
+    // if ready state not 4 we're exit on the func
+    if (request.readyState !== 4) return;
+
+    // http код ответа - бывает 404, 402.., нужно получить 200
+    if (request.status !== 200) {
+      console.log("error: " + request.status);
+      return;
+    }
+
+    const output = JSON.parse(request.responseText);
+
+    let inner = "";
+
+    output.results.forEach(item => {
+      let nameItem = item.name || item.title;
+      inner += `<div class='col-12'>${nameItem}</div>`;
+    });
+
+    movie.innerHTML = inner;
+
+    // console.log(output);
+    // console.log(request.responseText);
+    // console.log(request.readyState); // 4
+  });
+}
